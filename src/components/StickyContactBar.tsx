5f16e6409cb6a66cad5b0a54d352cb45d041e903
Dinh Khanh Tung <dinhkhanhtung@outlook.com>
2026-03-06 08:59:15 +0700

    Fix: Increase sticky contact bar distance from screen edge to prevent hiding

--- src/components/StickyContactBar.tsx
+++ src/components/StickyContactBar.tsx
@@ -6,7 +6,7 @@ const StickyContactBar = () => {
   const phoneLink = `tel:${phoneNumber}`;
 
   return (
-    <div className="fixed bottom-6 right-4 md:right-6 z-50 flex flex-col gap-3">
+    <div className="fixed bottom-6 right-6 md:right-8 z-50 flex flex-col gap-3">
       {/* Zalo Button */}
       {/* Zalo Button */}
       <a
